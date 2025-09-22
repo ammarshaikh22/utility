a90ac9a717ff1e3d0e0805d70ae0dbe6e8701871
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2025-09-22 18:21:36 +0500

    Done

--- app/Events/NewEstimateRequestEvent.php
+++ app/Events/NewEstimateRequestEvent.php
@@ -2,6 +2,7 @@
 
 namespace App\Events;
 
+// Import necessary classes
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
@@ -9,16 +10,19 @@ use Illuminate\Queue\SerializesModels;
 
 class NewEstimateRequestEvent
 {
+    // Use necessary traits for event handling
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
-    public $estimateRequest;
+    public $estimateRequest;  // The estimate request instance
 
     /**
      * Create a new event instance.
+     *
+     * @param mixed $estimateRequest
      */
     public function __construct($estimateRequest)
     {
+        // Initialize the estimateRequest property with the provided value
         $this->estimateRequest = $estimateRequest;
     }
-
 }
